c59a96c9d7e649eaa164ce5c87e5d4b8017f3da2
Daniel Yulius S <danielyuliussoetjipto@gmail.com>
2023-06-28 00:42:07 +0700

    handling request http #2

--- server.js
+++ server.js
@@ -1,35 +1,40 @@
 const http = require('http');
- 
+
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
-    
+
     //destruct parameter request = object
     const { method } = request;
- 
-    if(method === 'GET') {
+
+    if (method === 'GET') {
         response.end('<h1>Hello!</h1>');
     }
- 
-    if(method === 'POST') {
-        response.end('<h1>Hai!</h1>');
+
+    if (method === 'POST') {
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            
+            //parsing body yang string menjadi object yang formatnya {name: namanya}
+            const { name } = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}!</h1>`);
+        });
     }
- 
-    if(method === 'PUT') {
-        response.end('<h1>Bonjour!</h1>');
-    }
- 
-    if(method === 'DELETE') {
-        response.end('<h1>Salam!</h1>');
-    }
+
 };
- 
- 
+
+
 const server = http.createServer(requestListener);
- 
+
 const port = 5000;
 const host = 'localhost';
- 
+
 server.listen(port, host, () => {
     console.log(`Server berjalan pada http://${host}:${port}`);
 });
